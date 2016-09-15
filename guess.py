fb00bd67b515c03e92701547bc5c3ee896b9d6c5
Mahyar Babaie <mahyarhajibabaie@gmail.com>
2016-09-15 14:59:31 -0700

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+import sys
+import math
+from MyPRNG import *
+
+# EX. guess.py -h -v -s seed -m 1 -M 400
+
+def GuessHandler(userGuess,count, randomNumber):
+    # Takes care of current guess by storing it in an array and takes the absolute value of the difference
+    # Handles the previous guess as well to compare to see if it was warmer or colder
+    currentGuessDifference = userGuess[count] - randomNumber
+    currentGuessDifference = math.fabs(currentGuessDifference)
+    previousGuessDifference = userGuess[count - 1] - randomNumber
+    previousGuessDifference = math.fabs(previousGuessDifference)
+
+    # If the current guess is smaller then it will return true which means they are approaching the random number
+    if (currentGuessDifference < previousGuessDifference):
+        return True
+    else:
+        return False
+
+def main():
+    prng = MyPRNG()
+    count = 0
+    randomNumber = 0
+    verbose = False
+    loop = True
+    userGuess = []
+    minimum = 1
+    maximum = 1000
+    seed = 101
+    index = 0
+    hMessage = "This program will create a game which runs the PRNG to pick the 101st random number for the user to guess"
+
+
+    for currentArg in sys.argv:
+        if (currentArg == "-h"):
+            print (hMessage)
+        elif (currentArg == "-v"):
+            verbose = True
+        elif (currentArg == "-s"):
+            seed = int(sys.argv[index + 1])
+        elif (currentArg == "-m"):
+            minimum = int(sys.argv[index + 1])
+        elif (currentArg == "-M"):
+            maximum = int(sys.argv[index + 1])
+        index += 1
+
+    prng.seed(seed)
+    randomNumber = prng.next_prn()
+    while(loop):
+
