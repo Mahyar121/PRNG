d1985a7091d680b89c692e2b32964da2aaae67d9
Mahyar Babaie <mahyarhajibabaie@gmail.com>
2016-09-14 17:18:18 -0700

    Add files via upload

--- MyPRNG.py
+++ MyPRNG.py
@@ -2,13 +2,12 @@
 import sys
 
 class MyPRNG:
-    def __init__(self,a,m,z, m_seed, next): #zero-arg constructor for the class
+    def __init__(self): #zero-arg constructor for the class
         self.a = 16807 #7^5
         self.m = 2147483647 #2^31 - 1
-        self.nextZ = 1
-        self.currentZ = 0
+        self.currentZ = 1
     def next_prn(self): #return the next random number
-        self.nextZ = ( self.a * self.currentZ ) % self.m
-        return self.next
+        self.currentZ = ( self.a * self.currentZ ) % self.m
+        return self.currentZ
     def seed(self, rseed): # seed the generator with the given rseed
         self.currentZ = rseed
